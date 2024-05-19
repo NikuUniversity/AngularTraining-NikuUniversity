9861bef6de69ed70faa1c396b9acd9462097d3f8
NikuUniversity <NikuUniversity@gmail.com>
2024-05-19 14:58:01 +0530

    Component Added

--- angularbasics/src/app/app.module.ts
+++ angularbasics/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
